68613b612fe80a6bbae1308fc572ee94b0948b72
Andrewon <duc.tm8@gmail.com>
2021-01-06 10:23:47 -0600

    Each item is not touchable

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView,Image,TouchableOpacity, Button, Alert, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+  FlatList,
+  TouchableWithoutFeedback,
+  SectionList,
+} from "react-native";
 
-import DATA from "TestData";
+import { DATA } from "./src/TestData";
 
 const Stack = createStackNavigator();
+
+const Item = (
+  { title } //this is where the Item get display. need to look at this in detail
+) => (
+  <TouchableOpacity style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </TouchableOpacity>
+);
 
 export default function App() /**the 'export' is equivalent to run**/ {
   return (
@@ -19,39 +39,52 @@ export default function App() /**the 'export' is equivalent to run**/ {
   );
 }
 
-function HomeScreen({navigation}) {
-  return(
-  <SafeAreaView style={styles.container}>
-
-    <Text>Workout app test</Text>
-    <Button
-      title="Go to Details"
-      onPress={ () => navigation.navigate ('Details') }
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Workout app test</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
       />
 
-  </SafeAreaView>
-  )
-
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
 function DetailsScreen() {
-  return(
-  <SafeAreaView style={styles.container}>
-
-    <Text>Details</Text>
-
-  </SafeAreaView>
-  )
-
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Details</Text>
+    </SafeAreaView>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 20,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 16,
   },
 });
--- src/TestData.js
+++ src/TestData.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const DATA = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"]
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"]
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"]
-    }
-  ];
+export const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+];
